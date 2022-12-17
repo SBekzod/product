f91b58934c8dbf686a191e3da849c293d4fe0f29
jonik2909 <boronovjonibek@gmail.com>
2022-12-17 21:37:20 +0900

    fix: modification input design

--- create/index.php
+++ create/index.php
@@ -10,21 +10,20 @@ include_once($site_path . "/lib/navbar.php");
   <form action="/ajax/product_ajax.php" method="POST" enctype="multipart/form-data">
     <p class="text-center mt-3 fs-2">LET'S CREATE</p>
 
-    <div class="input-group mb-3">
-      <input type="text" class="form-control" name="product_name">
-      <span class="input-group-text">Name</span>
+    <div class="form-floating mb-3">
+      <input type="text" class="form-control" id="floatingInput"  placeholder="Name" name="product_name">
+      <label for="floatingInput">Name</label>
     </div>
-    <div class="input-group mb-3">
-      <input type="text" class="form-control" name="product_price" >
-      <span class="input-group-text">Price</span>
+    <div class="form-floating mb-3">
+      <input type="text" class="form-control" id="floatingInput"  placeholder="Price" name="product_price" >
+      <label for="floatingInput">Price</label>
     </div>
-    <div class="input-group mb-3">
-      <input type="text" class="form-control" name="product_creator" >
-      <span class="input-group-text">Creator</span>
+    <div class="form-floating mb-3">
+      <input type="text" class="form-control" id="floatingInput"  placeholder="Creator" name="product_creator" >
+      <label for="floatingInput">Creator</label>
     </div>
-    <div class="input-group mb-3">
-      <input type="file" class="form-control"  name="product_img">
-      <label class="input-group-text" >Upload</label>
+    <div class="form-floating mb-3">
+      <input type="file" class="form-control" id="floatingInput"  placeholder="Upload"  name="product_img">
     </div>
     <div class="d-flex justify-content-end">
       <button name="action" value="create_product" class="btn btn-primary">Save</button>
