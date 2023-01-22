44514ad82e6a4c7a38cac53cd6d9d9351232b912
martin <martin@comets.kr>
2023-01-23 00:52:12 +0900

    feat: MVC details

--- index.php
+++ index.php
@@ -9,15 +9,21 @@ $product = new Product($con);
 
 $products_data = $product->getAllProducts();
 
+// MVC => CMV => Controller & Model & View
+
 ?>
 
 
-
-
-
-
-
 <div class="container">
+
+  <div class="card" style="width: 18rem; margin: 30px">
+    <img src="https://media.cnn.com/api/v1/images/stellar/prod/221223090506-01-bugatti-chiron-profilee.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
 
   <h1 class="display-3 mt-5 text-center">Welcome to Apple Store</h1>
   <div class="d-flex flex-wrap">
